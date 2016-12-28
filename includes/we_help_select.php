6fc7f7b0b87dbd327939b90a7326e100f9b62103
unknown <anzarsh@mail.ru>
2016-12-28 10:23:08 +0300

    add all

--- includes/we_help_select.php
+++ includes/we_help_select.php
@@ -1,1 +1,10 @@
-Оставьте заявку и мы поможем выбрать <br>
+<section class="db-all-container">
+<div class="container">
+	<div class="row">
+		<div class="db-header">
+			span
+		</div>
+	</div>
+</div>
+	
+</section>
