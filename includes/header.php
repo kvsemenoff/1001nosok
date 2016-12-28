d51af769be40af3e453a6d1f6a49d73e62093401
Berzeg07 <berezg86@gmail.com>
2016-12-28 09:59:59 +0300

    29.14

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<section class="dd-header-wrap">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-9">
+				<div class="dd-logo">
+					<h3>1001NOSOK.RU</h3>
+				</div>
+				<div class="dd-phone">
+					
+				</div>
+			</div>
+			<div class="col-md-3">
+				<ul class="dd-contacts">
+					<li><a href="#">Доставка и гарантия</a></li>
+					<li><a href="#">Контакты</a></li>
+				</ul>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+</section>
