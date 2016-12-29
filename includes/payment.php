a72b6e4bbf0ebbad632100f1d025a189071bef99
unknown <anzarsh@mail.ru>
2016-12-29 11:25:52 +0300

    add index.php

--- includes/payment.php
+++ includes/payment.php
@@ -1,1 +1,33 @@
-Доставка и оплата<br>
+<section class="dd-dostavka">
+	<div class="my-container2">
+		<div class="dd-dostavka-content">
+			<div class="dd-title">
+				<span>Доставка и оплата</span>
+			</div>
+			<div class="dd-box dd-box1">
+				<div class="dd-box-txt">
+					<span>По Москве</span><br>
+					<span>Осуществляется курьерской службой.
+						Сроки доставки: 1-2 дня.</span>
+					</div>
+					<img class="img-responsive" src="img/moscow.jpg" alt="">
+				</div>
+				<div class="dd-box dd-box2">
+					<div class="dd-box-txt">
+						<span>По России</span><br>
+						<span>Осуществляется курьерской службой или почтой России<br>
+							(в зависимости от региона). Сроки доставки 2-12 дней.</span>
+						</div>
+						<img class="img-responsive" src="img/russia.jpg" alt="">
+					</div>
+					<div class="clearfix"></div>
+			</div>
+			
+		</div>
+		<div class="dd-big-txt">
+				<span>Стоимость доставки – от 350 р. (в зависимости от региона).<br>
+					Точные стоимость и сроки доставки обсуждаются индивидуально с менеджером при подтверждении заказа.<br>
+					Оплата производится наличными при получении заказа или предоплатой на банковский счет и электронные кошельки.</span>
+				</div>
+			</div>
+	</section>
